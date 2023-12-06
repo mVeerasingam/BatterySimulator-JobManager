14a0fffdb0935f9b276aaa5e82a80b5ab43ae168
Mark Veerasingam <markveer70@gmail.com>
2023-12-06 18:18:33 +0000

    uncomment docker url when building image!!!

--- src/main/java/com/example/batterysimjobmanager_prototype/update_battery/PyBaMM_SimulationClient.java
+++ src/main/java/com/example/batterysimjobmanager_prototype/update_battery/PyBaMM_SimulationClient.java
@@ -3,7 +3,8 @@ package com.example.batterysimjobmanager_prototype.update_battery;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-@FeignClient(name = "Simulation-Service", url = "http://battery-simulation-service:8084")
+@FeignClient(name = "Simulation-Service", url = "http://localhost:8084")
+//@FeignClient(name = "Simulation-Service", url = "http://battery-simulation-service:8084")
 public interface PyBaMM_SimulationClient {
     // Test Java updating Python battery simulation parameters
     @PostMapping("/simulate")
