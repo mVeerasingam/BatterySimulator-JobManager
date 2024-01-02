3ed30affd51a29cbe3a168d281038838d1f42507
Mark Veerasingam <markveer70@gmail.com>
2024-01-02 00:51:41 +0000

    Added RabbitMQ into docker-compose.yml

--- src/main/java/com/example/batterysimjobmanager_prototype/clients/PyBaMM_SimulationClient.java
+++ src/main/java/com/example/batterysimjobmanager_prototype/clients/PyBaMM_SimulationClient.java
@@ -6,8 +6,8 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 @Async
-@FeignClient(name = "Simulation-Service", url = "http://localhost:8084")
-//@FeignClient(name = "Simulation-Service", url = "http://battery-simulation-service:8084")
+//@FeignClient(name = "Simulation-Service", url = "http://localhost:8084")
+@FeignClient(name = "Simulation-Service", url = "http://battery-simulation-service:8084")
 public interface PyBaMM_SimulationClient {
     @PostMapping("/cell/simulate")
     String simulateCell(@RequestBody BatterySimMessage batterySim);
