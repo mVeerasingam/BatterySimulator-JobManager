4528604792419a3d9f009ead9fa57b90983cb285
Mark Veerasingam <markveer70@gmail.com>
2024-01-09 15:36:48 +0000

    updated docker-compose.yml and set everything to docker

--- src/main/java/com/example/batterysimjobmanager_prototype/clients/DbOperations_Client.java
+++ src/main/java/com/example/batterysimjobmanager_prototype/clients/DbOperations_Client.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Async
-@FeignClient(name = "Database-Connection-Service", url = "http://localhost:8082")
+@FeignClient(name = "Database-Connection-Service", url = "http://database:8082")
+//@FeignClient(name = "Database-Connection-Service", url = "http://localhost:8082")
 public interface DbOperations_Client {
     // Test Java updating Python battery simulation parameters
     @PostMapping("/createSimulation")
